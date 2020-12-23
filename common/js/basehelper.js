75b5877deb7195ab842b4f24b539c46778e21b47
User <10652364+dhkim0800@users.noreply.github.com>
2020-12-23 15:05:41 +0900

    Dynamic language loading

--- common/js/basehelper.js
+++ common/js/basehelper.js
@@ -13,3 +13,35 @@ function loadSlideshowSlides() {
         });
     })
 }
+
+async function loadLanguages() {
+    let data = await new Promise((resolve) => {
+        $.getJSON(langsPath, (data) => {
+            resolve(data);
+        });
+    });
+    for (let language of data) {
+        let elem = $("<a>");
+        elem.text(language.emoji)
+            .attr("href", `../${language.code}/index.html`)
+            .attr("title", language.name)
+            .addClass("languageflagbutton")
+            .appendTo($("#selectLanguages"));
+    }
+
+
+    console.log(data);
+    twemoji.parse($(".langChooser")[0], {
+        size: "svg",
+        ext: ".svg"
+    });
+    $(".langChooser").css("visibility", "visible");
+
+    $(".languageflagbutton").each((i, elem) => {
+        $(elem).addClass("languageflagbutton_loading");
+        $(elem).css("animation-delay", i * 150 + "ms");
+        setTimeout(() => {
+            $(elem).removeClass("languageflagbutton_loading")
+        }, i * 150 + 1000);
+    })
+}
